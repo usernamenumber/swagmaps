8d98e487c3757af85a83aa8aa965da3adfbe2fac
Brad Smith <usernamenumber@users.noreply.github.com>
2015-01-06 12:21:15 -0500

    Update urls.py

--- swag/urls.py
+++ swag/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'swag.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
-    url('proxytest','swag.views.proxy_test'),
     url(r'^(.+)$', 'swag.views.map_view'),
     url(r'^$', 'swag.views.map_list'),
 )
